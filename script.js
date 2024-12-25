8f0d66bccbd9bf0057807e4aabdc30860a07c16f
Юлия <yu.s.boiko5002@gmail.com>
2024-12-26 00:19:37 +0300

    project

--- script.js
+++ script.js
@@ -3,13 +3,11 @@ document.querySelector('.menu-toggle').addEventListener('click', function () {
     menuList.classList.toggle('active');
 });
 function selectCard(selectedCard) {
-    // ������� ����� 'selected' � ���� ��������
     const cards = document.querySelectorAll('.card2');
     cards.forEach(card => {
         card.classList.remove('selected');
     });
 
-    // ��������� ����� 'selected' � ��������� ��������
     selectedCard.classList.add('selected');
 }
 $(document).ready(function () {
@@ -23,22 +21,19 @@ $(document).ready(function () {
         infinite: false
     });
 
-    // ���������� ��������
     function updatePager() {
         const current = $carousel.slick('slickCurrentSlide') + 1;
         const total = $carousel.slick('getSlick').slideCount;
         $pager.text(`${current} / ${total}`);
     }
 
-    // �������������� ���������� ��������
     updatePager();
 
-    // ������� ��� ����� ������
+
     $carousel.on('afterChange', function () {
         updatePager();
     });
 
-    // ��������� ��������
     $('.prev').on('click', function () {
         $carousel.slick('slickPrev');
     });
@@ -60,9 +55,9 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 document.getElementById('feedback-form').addEventListener('submit', function (event) {
-    event.preventDefault(); // ������������� ������������ ��������
+    event.preventDefault();
 
-    const formData = new FormData(this); // �������� ������ �����
+    const formData = new FormData(this);
 
     fetch('https://formcarry.com/s/5VY-8uLrxnS', {
         method: 'POST',
@@ -74,14 +69,12 @@ document.getElementById('feedback-form').addEventListener('submit', function (ev
         .then(response => response.json())
         .then(data => {
             if (data.success) {
-                document.getElementById('response-message').innerText = '��������� ������� ����������!';
-                this.reset(); // ������� �����
-            } else {
-                document.getElementById('response-message').innerText = '��������� ������. ����������, ���������� ��� ���.';
+                document.getElementById('response-message').innerText = 'Сообщение успешно отправлено!';
+                this.reset();
             }
         })
         .catch(error => {
-            console.error('������:', error);
-            document.getElementById('response-message').innerText = '��������� ������. ����������, ���������� ��� ���.';
+            console.error('Ошибка:', error);
+            document.getElementById('response-message').innerText = 'Произошла ошибка. Пожалуйста, попробуйте еще раз.';
         });
 });
